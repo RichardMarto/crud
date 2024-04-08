8f3356c3445728bcb7af9518ce80d7101fcb3f01
ulissesjnr <sessilo@live.com>
2024-04-08 16:31:09 -0300

    v4

--- src/main/java/com/ulisses/Application.java
+++ src/main/java/com/ulisses/Application.java
@@ -1,15 +1,13 @@
 package com.ulisses;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
 
-@ComponentScan (basePackages = {"com.ulisses.controllers"})
 @SpringBootApplication
-@EnableAutoConfiguration
+@ComponentScan
 public class Application {
 
     public static void main(String[] args) {
